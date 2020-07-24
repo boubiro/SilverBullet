9350056d8e005eebc434e5359e5c034ac298fc99
MOHAMMAD <mohamm4dx@outlook.com>
2020-07-24 22:53:15 +0430

    1.0.1

--- OpenBullet/SB.cs
+++ OpenBullet/SB.cs
@@ -24,7 +24,7 @@ namespace OpenBullet
             Alerter = Alerter
         };
 
-        public static string Version => "1.0.0";
+        public static string Version => "1.0.1";
 
         // Block Mappings (including Plugins)
         public static List<(Type, Type, Color)> BlockMappings = new List<(Type, Type, Color)>();
